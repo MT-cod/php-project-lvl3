778d7941eae77d69c024fa20db071b761e71c330
MT-cod <mammmoth@list.ru>
2021-06-15 10:47:27 +0300

    final

--- app/Http/Controllers/Engine.php
+++ app/Http/Controllers/Engine.php
@@ -91,13 +91,15 @@ class Engine extends Controller
 
     public function update(Request $request): RedirectResponse
     {
+        Validator::make($request->all(), ['id' => 'required'])->validate();
+
         Validator::make($request->all(), ['id' => [new CheckConnectToUrl()]])->validate();
 
         $url_id = $request->input('id');
         $urlName = DB::table('urls')->where('id', $url_id)->value('name');
-        if (empty($urlName)) {
+        /*if (empty($urlName)) {
             return redirect()->route('urls.show', ['id' => $url_id]);
-        }
+        }*/
 
         //Повторно проверяем подключение и собираем инфу по тегам и пишем данные по проверке подключения
         /*try {*/
